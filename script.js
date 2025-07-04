2d2a6064955fccfabcbd79746b82da33a49f84b7
AntonioMatheus-dev <matheus69969@gmail.com>
2025-07-04 14:44:34 -0300

    Confirmaca_do_arquivo

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const dropImagem = document.querySelector(".arraste-aqui") as HTMLElement;
-const botaoArquivo = document.querySelector(".Botao_arquivo") as HTMLElement;
-const inputArquivo = document.getElementById("fileInput") as HTMLInputElement;
-const textoArraste = document.getElementById("texto-arraste") as HTMLElement;
-const arquivoInfo = document.getElementById("arquivo-info") as HTMLElement;
-const nomeArquivo = document.getElementById("nome-arquivo") as HTMLElement;
-
-function mostrarArquivo(nome: string) {
+var dropImagem = document.querySelector(".arraste-aqui");
+var botaoArquivo = document.querySelector(".Botao_arquivo");
+var inputArquivo = document.getElementById("fileInput");
+var textoArraste = document.getElementById("texto-arraste");
+var arquivoInfo = document.getElementById("arquivo-info");
+var nomeArquivo = document.getElementById("nome-arquivo");
+function mostrarArquivo(nome) {
     if (arquivoInfo && nomeArquivo && textoArraste) {
         nomeArquivo.textContent = nome;
         arquivoInfo.style.display = "flex";
@@ -15,33 +14,29 @@ function mostrarArquivo(nome: string) {
         botaoArquivo.classList.add("azul");
     }
 }
-
 if (dropImagem) {
-    dropImagem.addEventListener('dragover', (e: DragEvent) => {
+    dropImagem.addEventListener('dragover', function (e) {
         e.preventDefault();
         dropImagem.classList.add('dragover');
     });
-
-    dropImagem.addEventListener('dragleave', () => {
+    dropImagem.addEventListener('dragleave', function () {
         dropImagem.classList.remove('dragover');
     });
-
-    dropImagem.addEventListener('drop', (e: DragEvent) => {
+    dropImagem.addEventListener('drop', function (e) {
         e.preventDefault();
         dropImagem.classList.remove('dragover');
         if (e.dataTransfer && e.dataTransfer.files.length > 0) {
-            const file = e.dataTransfer.files[0];
+            var file = e.dataTransfer.files[0];
             if (file.type === "application/pdf") {
                 mostrarArquivo(file.name);
             }
         }
     });
 }
-
 if (inputArquivo) {
-    inputArquivo.addEventListener('change', () => {
+    inputArquivo.addEventListener('change', function () {
         if (inputArquivo.files && inputArquivo.files.length > 0) {
-            const file = inputArquivo.files[0];
+            var file = inputArquivo.files[0];
             if (file.type === "application/pdf") {
                 mostrarArquivo(file.name);
             }
