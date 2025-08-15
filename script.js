9b68139dc01f819402fc76a0686490fd1ecb0138
AntonioMatheus-dev <matheus69969@gmail.com>
2025-08-15 15:51:25 -0300

    Commit

--- script.js
+++ script.js
@@ -137,7 +137,12 @@ function mostrarErro() {
     botaoArquivo.classList.remove("azul");
     botaoArquivo.style.backgroundColor = "#DF2F47";
     botaoArquivo.innerHTML = "<strong>ERRO</strong>";
+    botaoArquivo.disabled = true;
     botaoArquivo.onclick = null;
+    botaoArquivo.setAttribute("tabindex", "-1");
+  }
+  if (inputArquivo) {
+    inputArquivo.disabled = true; // desativa o input real
   }
 
   // Mostra seta de voltar
